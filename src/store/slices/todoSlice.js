6674515cb5fff76995016e85299f0eb5641d32a1
MrLipa2496 <sasalipa216@gmail.com>
2024-08-12 11:06:54 +0300

    Add store & todoSlice

--- src/store/slices/todoSlice.js
+++ src/store/slices/todoSlice.js
@@ -1,1 +1,40 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  tasks: [],
+};
+
+const todoSlice = createSlice({
+  name: 'todos',
+  initialState,
+  reducers: {
+    addTask: (state, action) => {
+      state.tasks.push(action.payload);
+    },
+
+    toggleTask: (state, action) => {
+      const task = state.tasks.find(t => t.id === action.payload);
+      if (task) {
+        task.isDone = !task.isDone;
+      }
+    },
+
+    deleteTask: (state, action) => {
+      state.tasks = state.tasks.filter(t => t.id !== action.payload);
+    },
+
+    editTask: (state, action) => {
+      const { id, value, deadline } = action.payload;
+      const task = state.tasks.find(t => t.id === id);
+
+      if (task) {
+        task.value = value;
+        task.deadline = deadline;
+      }
+    },
+  },
+});
+
+export const { addTask, toggleTask, deleteTaskm, editTask } = todoSlice.actions;
+
+export default todoSlice.reducer;
